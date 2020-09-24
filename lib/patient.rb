678545aca32ad4076c57308a10aa90871aae3ac7
Travis Lee <travisslee223@gmail.com>
2020-09-24 03:28:42 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,3 +1,17 @@
 class Patient
   
+  attr_accessor :name, :doctor, :appointment
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @doctor = doctor
+    @appointment = appointment
+    @@all << self
+  end
+  
+  def new_appointment
+    
+  end
 end
